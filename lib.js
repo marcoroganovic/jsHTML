6fb5d4defd533c146953f036bdfb459547087456
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-06 22:52:18 +0100

    add ability to add missing HTML tags

--- lib.js
+++ lib.js
@@ -2,14 +2,14 @@ var jsHTML = (function() {
 
   "use strict";
 
-  var ELEMENTS = ['address', 'applet', 'area', 'article', 'aside', 'base', 
-    'basefont', 'bgsound', 'blockquote', 'body', 'br', 'button', 'caption', 
+  var ELEMENTS = ['a', 'address', 'applet', 'area', 'article', 'aside', 'base', 
+    'basefont', 'bgsound', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 
     'center', 'col', 'colgroup', 'dd', 'details', 'dir', 'div', 'dl', 'dt', 
     'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'frame', 
     'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 
     'hr', 'html', 'iframe', 'img', 'input', 'isindex', 'li', 'link', 'listing', 
     'main', 'marquee', 'menu', 'menuitem', 'meta', 'nav', 'noembed', 'noframes', 
-    'noscript', 'object', 'ol', 'p', 'param', 'plaintext', 'pre', 'script', 
+    'noscript', 'object', 'ol', 'p', 'param', 'plaintext', 'pre', 'span', 'script',
     'section', 'select', 'source', 'style', 'summary', 'table', 'tbody', 'td', 
     'template', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'track', 
     'ul', 'wbr', 'xmp'];
@@ -66,8 +66,16 @@ var jsHTML = (function() {
     }
   }
 
+  function addElement(tag) {
+    tag = tag.toLowerCase();
+    this[tag] = this[tag] || createNode(tag);
+    return this;
+  }
+
   var api = {};
+  api.addTag = addElement;
   ELEMENTS.forEach(tag => api[tag] = createNode(tag));
 
   return api;
+
 })();
